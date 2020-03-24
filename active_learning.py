f0e383f7951ac7d49dfd47dfcaef42eb8f2a603e
Sam Clarke <samckclarke@gmail.com>
2020-03-24 13:57:51 +1300

    acqs_per_round is now an arg (constant over all rounds)

--- active_learning.py
+++ active_learning.py
@@ -1,13 +1,13 @@
 import random
 
-def acq_random(train_data, pool_idx, model, n_acqs=10):
+def acq_random(train_data, pool_idx, model, args):
     """
     Random acquisition baseline.
     """
-    new_idx = random.sample(pool_idx, k=n_acqs) # random sample without replacement
+    new_idx = random.sample(pool_idx, k=args.acqs_per_round) # random sample without replacement
     return set(new_idx)
 
-def acq_BALD(train_data, pool_idx, model, n_acqs=10):
+def acq_BALD(train_data, pool_idx, model, args):
     for i in pool_idx:
         point = train_data[i]
         # apply BALD on point and record info gain
