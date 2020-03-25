c54a46f8d178cdc435f7fa7af0e9387b140f7474
Sam Clarke <samckclarke@gmail.com>
2020-03-25 13:56:36 +1300

    Add acq_max_ent function (entropy calc currently untested)

--- active_learning.py
+++ active_learning.py
@@ -1,20 +1,86 @@
-import random
+import random, torch, time, logging
+import numpy as np
+from utils import make_dataloader
 
 def acq_random(train_data, pool_idx, model, args):
     """
     Random acquisition baseline.
     """
     new_idx = random.sample(pool_idx, k=args.acqs_per_round) # random sample without replacement
-    return set(new_idx)
+    return set(new_idx), None
 
 def acq_BALD(train_data, pool_idx, model, args):
-    for i in pool_idx:
-        point = train_data[i]
-        # apply BALD on point and record info gain
-    # return 10 idx that maximise BALD
-
-def acq_max_ent():
     pass
+
+def acq_max_ent(train_data, pool_idx, model, args):
+    """
+    Chooses pool points that maximise the predictive entopy (Shannon, 1948).
+    Returns indices of the top `args.acqs_per_round` points from the pool.
+    The elements of train_data at pool_idx are the "pool points".
+
+    Parameters
+    ----------
+    train_data: torch.utils.data.Dataset
+        PyTorch dataset, superset of the pool data
+    pool_idx: set
+        indices specifying which points in train_data are in the pool
+    model: torch.nn.Module
+        find prective entropy of points wrt this PyTorch model
+    args: Namespace object
+        experiment arguments from argparse
+
+    Returns
+    -------
+    new_idx: set
+        indices of pool points which maximise predictive entropy
+        len(new_idx) = args.acqs_per_round
+    mean_entropy: float
+        the mean entropy of those selected pool points
+    """
+    best_ent_idx = np.zeros(shape=(0,2), dtype=np.float64) # array for storing top 10 (entropy, idx) pairs
+    start = time.time()
+    pool_loader = make_dataloader(train_data, pool_idx, args.test_batch_size, random=False) # note 1
+    with torch.no_grad():
+        for data, _, idx in pool_loader:
+            output = model.forward_stochastic(data, k=args.dropout_samples).double() # do entropy calcs in double precision
+            # mean over dropout samples
+            log_p_yc_xD = output.mean(dim=-1)
+            # compute entropy and sum over class dimension TODO write some checks to verify entropy calculation
+            H_y_xD = - (log_p_yc_xD * log_p_yc_xD.exp()).sum(dim=-1).numpy()
+            # add new (entropy, index) tuples to array of best so far
+            import ipdb;ipdb.set_trace()
+            new_ent_idx = np.column_stack((H_y_xD, idx))
+            all_ent_idx = np.concatenate((new_ent_idx, best_ent_idx), axis=0)
+            # sort by entropy and take top 10 so far
+            sorted_ind = all_ent_idx[:,0].argsort()
+            best_ent_idx = all_ent_idx[sorted_ind][-args.acqs_per_round:]
+            logging.info('Computing entropy for points with (original) indices {}-{} in pool'.format(
+                idx[0], idx[-1]))
+
+    # Old code
+    # for i in pool_idx:
+    #     data, _ = train_data[i]
+    #     data = data.unsqueeze(0) # create a batch dim, so data.shape = [1,1,28,28]
+    #     output = model.forward_stochastic(data, k=args.dropout_samples)
+    #     assert output.shape == (1, 10, args.dropout_samples) # only verified correctness for single batch computation
+    #     # mean over dropout samples
+    #     log_p_yc_xD = output.mean(dim=-1)
+    #     # compute entropy and sum over class dimension TODO write some checks to verify entropy calculation
+    #     H_y_xD = - (log_p_yc_xD * log_p_yc_xD.exp()).sum(dim=-1).item()
+    #     # store entropy and index if it's in the top args.acqs_per_round
+    #     threshold_entropy, _ = top_entropies_and_idx[0] # threshold_entropy is the min of the args.acqs_per_round max entropies
+    #     if H_y_xD > threshold_entropy:
+    #         top_entropies_and_idx[0] = (H_y_xD, i)
+    #         top_entropies_and_idx.sort()
+    #     if i % 1000 == 0:
+    #         logging.info('Computing entropy for point {} in pool'.format(i))
+    
+    assert best_ent_idx.shape == (args.acqs_per_round, 2)
+    end = time.time()
+    logging.info("Time taken for {} acquisitions: {}".format(args.acqs_per_round, end - start))
+    mean_entropy = best_ent_idx[:,0].mean()
+    new_idx = set(best_ent_idx[:,1].astype(int))
+    return new_idx, mean_entropy
 
 def acq_mean_std():
     pass
