3dbc5e93d00a9298d1ced25d5e75ba02b629d576
Sam Clarke <samckclarke@gmail.com>
2020-03-26 15:58:04 +1300

    Fix typo

--- exp1.sh
+++ exp1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python main.py --rand_acq
+python main.py --random_acq
 python main.py --acq_func_ID info_gain
 python main.py --acq_func_ID max_ent
 python main.py --acq_func_ID mean_std
